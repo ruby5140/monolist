6a0353e3c1ff47c92253edb730db3dad83e4230f
ruby5140 <day.one_2006@icloud.com>
2017-12-23 03:36:35 +0000

    rakuten api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,28 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-   email = params[:session][:email].downcase
-   password = params[:session][:password]
-   if login(email, password)
-     flash[:success] = "ログインに成功しました"
-     #users#show へとリダイレクト
-     redirect_to @user
-   else
-     flash.now[:danger] = "ログインに失敗しました"
-     #sessions/new.html.erb を再表示
-     render "new"
-   end
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email, password)
+      flash[:success] = 'ログインに成功しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render 'new'
+    end
   end
 
   def destroy
-    session[:user_id] = nil
-    flash[:success] = "ログアウトしました。"
-    redirect_to root_url
   end
-  
+
   private
-  
-  def login(email,password)
+
+  def login(email, password)
     @user = User.find_by(email: email)
     if @user && @user.authenticate(password)
       # ログイン成功
