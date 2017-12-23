a60af4068825de1431c8e41c67424d75fd0b5641
ruby5140 <day.one_2006@icloud.com>
2017-12-23 07:09:55 +0000

    monolist

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,26 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   #暗号化
   has_secure_password
+  
+  has_many :ownerships
+  has_many :items, through: :ownerships
+  
+  #Want した Item だけ」を user.want_items で取得
+  has_many :wants
+  has_many :want_items, through: :wants, class_name: 'Item', source: :item
+  
+  def want(item)
+    self.wants.find_or_create_by(item_id: item.id)
+  end
+
+  def unwant(item)
+    want = self.wants.find_by(item_id: item.id)
+    want.destroy if want
+  end
+
+  def want?(item)
+    self.want_items.include?(item)
+  end
+
 end
+
